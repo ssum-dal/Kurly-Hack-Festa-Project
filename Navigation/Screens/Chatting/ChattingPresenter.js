b86f2737796e64dfd65ad889d829a9a2da77ade3
cheonyeji <ssaltuck@gmail.com>
2022-08-13 14:48:36 +0900

    chatting page w/o keyboard height setting

--- Navigation/Screens/Chatting/ChattingPresenter.js
+++ Navigation/Screens/Chatting/ChattingPresenter.js
@@ -1,11 +1,29 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, {useState} from "react";
+import { View, StyleSheet, Text, KeyboardAvoidingView } from "react-native";
+import Header from "../../../Components/Header/Header";
+import Input from "../../../Components/Input/Input";
+import Menu from "../../../Components/Input/Menu";
+
+const s = StyleSheet.create({
+    ChattingView: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+    },
+    ChattingHistoryView: {
+        flex: 1,
+    }
+});
 
 export default() => {
-
+    const [isMenuOpened, setIsMenuOpened] = useState(false);
     return (
-        <View>
-            <Text>Chatting</Text>
-        </View>
+        <KeyboardAvoidingView style={s.ChattingView}>
+            <Header title={'메시지 발송'}/>
+            <View style={s.ChattingHistoryView}>
+                <Text>이전 메세지 내역 보이는 창</Text>
+            </View>
+            <Input isMenuOpened={isMenuOpened} setIsMenuOpened={setIsMenuOpened} />
+            {isMenuOpened ? <Menu /> : null}
+        </KeyboardAvoidingView>
     )
 }
