60f8ea212f0789a52e0db38d6669365133f5bbd8
ssum-dal <min8226@gmail.com>
2022-08-20 13:06:15 +0900

    메시지 리스트 화면 구성

---
+++ Components/DeliveryInfo/DeliveryInfoMessage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+// 메시지의 배송 정보
+
+const s = StyleSheet.create({
+    InfoTextView: {
+        backgroundColor: '#FAFAFA',
+        marginVertical: '2%',
+        marginHorizontal: '5%'
+    },
+    TrackingNumber: {
+        fontWeight: 'bold',
+        marginBottom: '2%'
+    },
+});
+
+function DeliveryInfoMessage({ navigation, trakingNum, name, address, category, detail, uri }) {
+    return (
+        <TouchableOpacity
+            activeOpacity={1}
+            onPress={() => {
+                navigation.push('Chatting');
+            }}
+        >
+            <View style={s.InfoTextView}>
+                <Text style={s.TrackingNumber}>{trakingNum}</Text>
+                <Text>받는 분 : {name}</Text>
+                <Text>배송지 : {address}</Text>
+                <Text>문의사항 : {category}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+export default DeliveryInfoMessage;
--- Navigation/Screens/Message/MessagePresenter.js
+++ Navigation/Screens/Message/MessagePresenter.js
@@ -1,11 +1,67 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
+import Header from "../../../Components/Header/Header";
+import DeliveryInfoMessage from "../../../Components/DeliveryInfo/DeliveryInfoMessage";
 
-export default() => {
+const info = [
+    {
+        id: 123456121212, 
+        name: '서수민', 
+        address: '서울특별시 강서구 마곡중앙로 36 어쩌구 저쩌구', 
+        category: '배송사진이 희망장소와 달라요',
+        detail: '배송 희망 장소로 집 앞을 선택하였는데 사진 속 위치는 알 수 없는 주차장입니다. 첨부한 사진 위치로 다시 배송해주세요.',
+        uri: 'https://cdn.pixabay.com/photo/2021/08/25/07/21/cat-6572630_960_720.jpg'
+    },
+    {   
+        id: 151548484848, 
+        name: '달래', 
+        address: '서울특별시 강서구 마곡중앙로 36 어쩌구 저쩌구', 
+        category: '요청과 다르게 배송',
+        detail: '배송 희망 장소로 집 앞을 선택하였는데 사진 속 위치는 알 수 없는 편의점입니다. 첨부한 사진 위치로 다시 배송해주세요.',
+        uri: ''
+    },
+    {
+        id: 454515151544, 
+        name: '천예찌', 
+        address: '서울특별시 강서구 마곡중앙로 36 어쩌구 저쩌구', 
+        category: '요청과 다르게 배송',
+        detail: '배송 희망 장소로 집 앞을 선택하였는데 사진 속 위치는 알 수 없는 집입니다. 첨부한 사진 위치로 다시 배송해주세요.',
+        uri: ''
+    },
+
+]
+
+const s = StyleSheet.create({
+    MessageView: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+    },
+});
+
+export default({ navigation }) => {
+
+    const renderDeliveryInfo = ({item, index}) => {
+        return (
+            <DeliveryInfoMessage
+                navigation={navigation}
+                trakingNum={item.id}
+                name={item.name}
+                address={item.address}
+                category={item.category}
+                detail={item.detail}
+            />
+        );
+    }
 
     return (
-        <View>
-            <Text>배송 리스트</Text>
+        <View style={s.MessageView}>
+            <Header title={'메시지'}/>
+            
+            <FlatList
+                data={info}
+                renderItem={renderDeliveryInfo}
+                keyExtractor={(item) => String(item.id)}
+            />
         </View>
     )
 }
