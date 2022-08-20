62eaae901f5293da7ff022ed89fbd350db97599d
cheonyeji <ssaltuck@gmail.com>
2022-08-20 13:11:17 +0900

    FCM setting

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./Navigation/Navigator/RootNavigator";
+import { NotificationListener, requestUserPermission } from "./src/utils/pushnotification_helper";
 
 const App = () => {
+	useEffect(()=> {
+		requestUserPermission();
+		NotificationListener();
+	}, []);
+
 	return (
 	    <NavigationContainer>
             <RootNavigator />
