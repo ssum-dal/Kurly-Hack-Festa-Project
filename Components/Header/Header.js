73990de9c843c86f19e539146ea64b906bce8cc2
ssum-dal <min8226@gmail.com>
2022-08-22 03:36:09 +0900

    헤더 뒤로가기 버튼 추가, 스타일 변경

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,23 +1,36 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const s = StyleSheet.create({
     HeaderView: {
         alignItems: 'center',
-        backgroundColor: '#5F0080',
-        paddingVertical: '4%'
+        justifyContent: 'space-between',
+        backgroundColor: '#ffffff',
+        paddingVertical: '2%',
+        flexDirection: 'row'
     },
     TextView: {
         fontSize: 16,
         fontWeight: 'bold',
-        color: '#ffffff'
+        color: '#000000'
     }
 });
 
 function Header({ title }) {
+    const navigation = useNavigation();
+
     return (
         <View style={s.HeaderView}>
+            <TouchableOpacity
+                activeOpacity={1}
+                onPress={() => navigation.goBack()}
+            >
+                <Icon name="chevron-back" color="#000000" size={30} style={{padding:5}}/>
+            </TouchableOpacity>
             <Text style={s.TextView}>{title}</Text>
+            <Icon name="chevron-back" color="#ffffff" size={30}/>
         </View>
     );
 }
